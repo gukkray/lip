539a34859992126845d1eadbd4fc5fe2a333070a
Rayssa <raymanoella8812@gmail.com>
2026-04-16 20:21:42 -0300

    atualização

--- aulas/cap4/3.py
+++ aulas/cap4/3.py
@@ -2,6 +2,11 @@ import time
 contador = 10
 
 while(contador > 0):
-    print(contador, end=', ')
+
+    if (contador == 1):
+        print(contador)
+        break
+
+    print(contador, end=', ', flush=True)
     contador = contador- 1
     time.sleep(1)
